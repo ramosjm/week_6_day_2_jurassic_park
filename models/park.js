57039b8363fb4a5ade8dcaf442ccdc3f9167adcd
ramosjm <jjramos_2004@hotmail.com>
2019-02-19 22:33:16 +0000

    park attract most visitors.

--- models/park.js
+++ models/park.js
@@ -2,6 +2,18 @@ const Park = function (name, ticketPrice, dinosaurs = []) {
   this.name = name;
   this.ticketPrice = ticketPrice;
   this.dinosaurs = dinosaurs;
-}
+};
+
+
+Park.prototype.mostVisitors = function(){
+  this.dinosaurs.sort(function(a, b){return b.guestsAttractedPerDay - a.guestsAttractedPerDay});
+  return this.dinosaurs[0];
+};
+
+Park.prototype.findSpecies = function(species){
+  result = this.dinosaurs.species.filter(species);
+  console.log(result);
+  return result;
+};
 
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -6,12 +6,19 @@ describe('Park', function() {
 
   let park;
   let dinosaur;
+  let bigPark;
+  let dinosaurGroup;
 
   beforeEach(function () {
     park = new Park('Gretna',3.50,[]);
-    dinosaur = new Dinosaur('Orc','Grass',20)
-
-  })
+    dinosaur = new Dinosaur('Orc','Grass',20);
+    dinosaur1 = new Dinosaur('Elf','Grass',10);
+    dinosaur2 = new Dinosaur('Elf','Meat',10);
+    dinosaur3 = new Dinosaur('Elf','Meat',20);
+    dinosaur4 = new Dinosaur('Goblin','Everything',30);
+    dinosaurGroup = [dinosaur,dinosaur1,dinosaur2,dinosaur3,dinosaur4];
+    bigPark = new Park ('Collossus',10,dinosaurGroup);
+  });
 
   it('should have a name',function(){
     const actual = park.name;
@@ -34,11 +41,26 @@ describe('Park', function() {
     assert.strictEqual(actual,1);
   });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to remove a dinosaur from its collection',function(){
+    park.dinosaurs.push(dinosaur);
+    park.dinosaurs.push(dinosaur1);
+    park.dinosaurs.push(dinosaur2);
+    park.dinosaurs.pop();
+    const actual = park.dinosaurs.length;
+    assert.strictEqual(actual,2);
+  });
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it('should be able to find the dinosaur that attracts the most visitors',function(){
+    const actual = bigPark.mostVisitors();
+    const expected = dinosaur4;
+    assert.strictEqual(actual,expected);
+  });
 
-  it('should be able to find all dinosaurs of a particular species');
+  it('should be able to find all dinosaurs of a particular species',function(){
+    const actual = bigPark.findSpecies('Elf');
+    const expected =[dinosaur1,dinosaur2,dinosaur3];
+    assert.deepStrictEqual(actual,expected);
+  });
 
   it('should be able to remove all dinosaurs of a particular species');
 
